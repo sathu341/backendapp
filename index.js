4d565a8f7c5422c5b0728f57fc04c3dc95a2a0c7
sathu341 <77712619+sathu341@users.noreply.github.com>
2024-11-19 09:23:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const  express=require('express')
 const app=express()
 const cors=require('cors')
-app.use(cors())
+app.use(cors(
+    { origin:["https://vercel.com/sathu341s-projects/clientapplink"],
+    method:["POST","GET"],
+    credentials:true
+    }
+))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 const mongoose=require('mongoose')
